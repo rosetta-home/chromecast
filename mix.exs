bf0eadae6e6c3e0900b2c8fa398e1198d6f1f206
Christopher Cote <chris@entropealabs.com>
2016-10-29 14:11:40 -0500

    make sure to include protobuf app

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Chromecast.Mixfile do
 
   def project do
     [app: :chromecast,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -16,7 +16,7 @@ defmodule Chromecast.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :ssl]]
+    [applications: [:logger, :ssl, :exprotobuf, :poison]]
   end
 
   def description do
